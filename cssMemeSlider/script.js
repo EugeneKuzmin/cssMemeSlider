aa75164741841aece34814946c72cbc9a385897c
Gennarro@yandex.ru <Gennarro@yandex.ru>
2023-09-16 19:17:15 +0300

    feat:add controls functionality and style  (Sat, Sep 16, 2023 07:17:15 PM)

---
+++ cssMemeSlider/script.js
@@ -0,0 +1,24 @@
+const carousel = document.getElementById('carousel')
+
+const picClass = document.querySelectorAll('.card__img')[0]
+const picWidth = picClass.clientWidth
+
+let activeIndex = 0
+
+const scrollWidth = carousel.scrollWidth - carousel.clientWidth
+
+const dots = document.querySelectorAll('.dot')
+
+const getDescription = (index) => {
+    return descriptions[index]
+}
+
+dots.forEach((dot,index)=>{
+    dot.addEventListener('click',()=>{
+        carousel.scrollLeft = picWidth*index
+        dots[activeIndex].classList.remove('active')
+        dot.classList.add('active')
+        activeIndex = index
+    }
+    )
+})
